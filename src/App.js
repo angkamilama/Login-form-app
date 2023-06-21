afb36d2df322a1fdcb56038288c9c1e00178cad2
angkamilama <angkami46@gmail.com>
2023-06-21 09:59:13 +0200

    react form app

--- src/App.js
+++ src/App.js
@@ -1,24 +1,98 @@
-import logo from './logo.svg';
-import './App.css';
+import React, { useRef, useState } from "react";
+import style from "./style.css";
 
 function App() {
+  const nameRef = useRef();
+  const passwordRef = useRef();
+  const [showUserError, setShowUserError] = useState(false);
+  const [showPasswordError, setShowPasswordError] = useState(false);
+  const [showPassword, setShowPassword] = useState(false);
+
+  const handleForm = (e) => {
+    e.preventDefault();
+    if (nameRef.current.value === "") {
+      setShowUserError((showUserError) => !showUserError);
+    }
+    if (passwordRef.current.value === "") {
+      setShowPasswordError((showPasswordError) => !showPasswordError);
+    }
+  };
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <>
+      <div className="container">
+        <h1 className="main-heading">inCard</h1>
+        <div className="form-container">
+          <h1>Login</h1>
+          <form onSubmit={handleForm}>
+            <label className="username-container">
+              <p>Username</p>
+              <input
+                type="email"
+                ref={nameRef}
+                onClick={() => {
+                  setShowUserError(false);
+                }}
+                placeholder="Enter your username..."
+              />
+              {showUserError ? (
+                <p className="input-error">A username is required</p>
+              ) : null}
+            </label>
+            <label>
+              <p>Password</p>
+              {showPassword ? (
+                <div className="password-container">
+                  <input
+                    type="text"
+                    onClick={() => {
+                      setShowPasswordError(false);
+                    }}
+                    ref={passwordRef}
+                    placeholder="Enter your password..."
+                  />
+                  <button
+                    className="show-btn"
+                    onClick={(e) =>
+                      setShowPassword((showPassword) => !showPassword)
+                    }
+                  >
+                    Hide
+                  </button>
+                </div>
+              ) : (
+                <div className="password-container">
+                  <input
+                    type="password"
+                    ref={passwordRef}
+                    onClick={() => {
+                      if (showPasswordError) {
+                        setShowPasswordError(false);
+                      }
+                    }}
+                    placeholder="Enter your password..."
+                  />
+                  <button
+                    className="show-btn"
+                    onClick={() =>
+                      setShowPassword((showPassword) => !showPassword)
+                    }
+                  >
+                    Show
+                  </button>
+                </div>
+              )}
+              {showPasswordError ? (
+                <p className="input-error">A password is required</p>
+              ) : null}
+            </label>
+            <button type="submit" className="login-btn">
+              Login
+            </button>
+          </form>
+        </div>
+      </div>
+    </>
   );
 }
 
